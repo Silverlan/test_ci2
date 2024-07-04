642aa693058fa97291ece6719c9fea7284cf1d24
Silverlan <silverlann@gmail.com>
2024-07-04 19:32:39 +0200

    f

---
+++ pragma_override/lua/test_ci.lua
@@ -0,0 +1,5 @@
+local test = "Hello"
+print("Test Message: ", test)
+print(engine.load_library("mount_external/pr_mount_external"))
+--print(\"Import: \",asset.import(\"headcrabclassic\", asset.TYPE_MODEL))
+game.load_model("headcrabclassic")
