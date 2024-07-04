97e82e99d6cf218c1d602da1a06f4efc6157d7ee
Silverlan <silverlann@gmail.com>
2024-07-04 19:44:31 +0200

    f

--- pragma_override/lua/test_ci.lua
+++ pragma_override/lua/test_ci.lua
@@ -3,3 +3,5 @@ print("Test Message: ", test)
 print(engine.load_library("mount_external/pr_mount_external"))
 --print(\"Import: \",asset.import(\"headcrabclassic\", asset.TYPE_MODEL))
 game.load_model("headcrabclassic")
+
+return false, "This is a test error"
